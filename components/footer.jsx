f351d5a3849f458c53c721029b2b6d95dafabc9e
Mihriban Alp <mihrilp@gmail.com>
2021-11-07 14:25:11 +0300

    feat: add footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import styles from "../styles/footer.module.scss";
+import Facebook from "../public/assets/facebook.svg";
+import Instagram from "../public/assets/instagram.svg";
+import Twitter from "../public/assets/twitter.svg";
+import { Logo } from "../components/icons";
 
 function Footer(props) {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Logo />
+      </div>
+      <div className={styles.follow}>
+        <h4>Follow Us: </h4>
+        <div>
+          <Facebook className={styles.icon} />
+          <Instagram className={styles.icon} />
+          <Twitter className={styles.icon} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
