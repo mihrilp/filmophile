2caedd6d76310945f6e1028f36429462dcbf52ec
Mihriban Alp <mihrilp@gmail.com>
2021-11-10 16:55:36 +0300

    style: styled detail page

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Image from "next/image";
+import styles from "../styles/detail.module.scss";
 
 export async function getStaticPaths() {
   const { data } = await axios(
@@ -28,35 +29,36 @@ export async function getStaticProps({ params }) {
 function MovieDetail({ movie }) {
   console.log(movie);
   return (
-    <div>
-      <h2>{movie.original_title}</h2>
+    <div className={styles.container}>
+      <h2 className={styles.title}>{movie.original_title}</h2>
       <Image
-        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-        alt="movie image"
-        width={300}
-        height={400}
-        layout="responsive"
-      />
-      <p>{movie.overview}</p>
-      <div className="genres">
-        {movie.genres.map((genre) => (
-          <p key={genre.id}>{genre.name}</p>
-        ))}
-      </div>
-      <Image
+        className={styles.image}
         src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
         alt="movie image"
-        width={300}
+        width={500}
         height={400}
-        layout="responsive"
       />
-      <div className="productionCompanies">
-        {movie.production_companies.map((production_company) => (
-          <p key={production_company.id}>{production_company.name}</p>
-        ))}
+      <div className={styles.scoreAndDate}>
+        <p>{movie.release_date}</p>
+        <p className={styles.score}>{movie.vote_average}</p>
       </div>
-      <p>{movie.release_dat}</p>
-      <p>{movie.vote_average}</p>
+      <p className={styles.overview}>{movie.overview}</p>
+      <div className={styles.moreInfo}>
+        <div className="productionCompanies">
+          <h3>Production Companies: </h3>
+          <br />
+          {movie.production_companies.map((production_company) => (
+            <p key={production_company.id}>{production_company.name}</p>
+          ))}
+        </div>
+        <div className={styles.genres}>
+          <h3>Genres: </h3>
+          <br />
+          {movie.genres.map((genre) => (
+            <p key={genre.id}> {genre.name} </p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
