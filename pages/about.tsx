a5212fef6029ff0727de57714a4f7f591f902201
mihrilp <mihribanalp.05@gmail.com>
2023-02-25 16:00:19 +0300

    feat: change about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,15 +6,14 @@ function About() {
       <Head>
         <title>About</title>
       </Head>
-      <p className="about__title">
-        A unique place for TV series and movie lovers <br /> (AKA Filmophiles)
-      </p>
+      <p className="about__title">What is Filmophile?</p>
       <p className="about__text">
-        You can see popular and top rated movies. You can find all details about
-        movies and Tv series like IMDB rates, date, subject etc. Also you can
-        watch trailer. You can search your favorite movie, TV series and actors.
-        At the bottom of the page you can see the movies and TV series that you
-        have visited before.
+        A unique place for TV series and movie lovers. You can check trends of
+        the week. You can see popular and top rated movies. You can see popular
+        and top rated Tv Series. You can find all details about movies and Tv
+        series like rates, date, story etc. Also you can watch trailer. You can
+        search your favorite movie, TV series and actors. At the bottom of the
+        page you can see the movies and TV series that you have visited before.
       </p>
     </div>
   );
