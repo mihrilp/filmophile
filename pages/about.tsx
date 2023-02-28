62a1c941fe6916cb1f60d225196b0c7874772c65
mihrilp <mihribanalp.05@gmail.com>
2023-02-28 13:11:54 +0300

    fix: responsive about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,9 +11,12 @@ function About() {
         A unique place for TV series and movie lovers. You can check trends of
         the week. You can see popular and top rated movies. You can see popular
         and top rated Tv Series. You can find all details about movies and Tv
-        series like rates, date, story etc. Also you can watch trailer. You can
-        search your favorite movie, TV series and actors. At the bottom of the
-        page you can see the movies and TV series that you have visited before.
+        series like rates, date, story etc. Also you can watch trailer.
+      </p>
+      <p className="about__text">
+        You can search your favorite movie, TV series and actors. At the bottom
+        of the page you can see the movies and TV series that you have visited
+        before.
       </p>
     </div>
   );
